bb053999202c7dfef71453e62ba51e44228174aa
akashmadduru <akashmadduru@gmail.com>
2022-07-04 18:06:51 +0530

    fix refactoring

--- lib/screen/main/main.dart
+++ lib/screen/main/main.dart
@@ -62,88 +62,96 @@ class _MainScreenState extends State<MainScreen> {
       key: scaffoldKey,
       drawerEnableOpenDragGesture: false,
       drawer: Drawer(child: SideBar()),
-      body: ListView(
-        scrollDirection: Axis.vertical,
-        children: <Widget>[
-          Topbar(
-            popSideBar: () => scaffoldKey.currentState?.openDrawer(),
+      body: Container(
+          child: ListView(
+            scrollDirection: Axis.vertical,
+            children: <Widget>[
+              Topbar(
+                popSideBar: () => scaffoldKey.currentState?.openDrawer(),
+              ),
+              Container(
+                padding: EdgeInsets.fromLTRB(20, 20, 0, 0),
+                child: Text(
+                  "Mint now",
+                  style: GoogleFonts.poppins(
+                      fontSize: 22,
+                      fontWeight: FontWeight.w800,
+                      color:
+                          context.watch<ThemeProvider>().getPriamryFontColor()),
+                ),
+              ),
+              Container(
+                  height: 205,
+                  width: 300,
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: mintNow.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return MintNowItem(
+                        id: index,
+                        url: mintNow[index]["url"].toString(),
+                        name:
+                            mintNow[index]["name"].toString().substring(0, 10) +
+                                "...",
+                        price: 2,
+                      );
+                    },
+                  )),
+              Container(
+                padding: EdgeInsets.fromLTRB(20, 20, 0, 0),
+                child: Text(
+                  "Top Movers",
+                  style: GoogleFonts.poppins(
+                      fontSize: 22,
+                      fontWeight: FontWeight.w800,
+                      color:
+                          context.watch<ThemeProvider>().getPriamryFontColor()),
+                ),
+              ),
+              TopMoversRowList(topMovers: topMoversMock),
+              Container(
+                padding: EdgeInsets.fromLTRB(20, 20, 0, 0),
+                child: Text(
+                  "Live Drops",
+                  style: GoogleFonts.poppins(
+                      fontSize: 22,
+                      fontWeight: FontWeight.w800,
+                      color:
+                          context.watch<ThemeProvider>().getPriamryFontColor()),
+                ),
+              ),
+              LiveDropRowList(liveDrops: liveDrops),
+              Container(
+                padding: EdgeInsets.fromLTRB(20, 20, 0, 0),
+                child: Text(
+                  "Unique Items",
+                  style: GoogleFonts.poppins(
+                      fontSize: 22,
+                      fontWeight: FontWeight.w800,
+                      color:
+                          context.watch<ThemeProvider>().getPriamryFontColor()),
+                ),
+              ),
+              Container(
+                  height: 205,
+                  width: 300,
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: mintNow.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return MintNowItem(
+                        id: index,
+                        url: mintNow[index]["url"].toString(),
+                        name:
+                            mintNow[index]["name"].toString().substring(0, 10) +
+                                "...",
+                        price: 2,
+                      );
+                    },
+                  )),
+            ],
           ),
-          Container(
-            padding: EdgeInsets.fromLTRB(20, 20, 0, 0),
-            child: Text(
-              "Mint now",
-              style: GoogleFonts.poppins(
-                  fontSize: 22,
-                  fontWeight: FontWeight.w800,
-                  color: context.watch<ThemeProvider>().getPriamryFontColor()),
-            ),
-          ),
-          Container(
-              height: 205,
-              width: 300,
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: mintNow.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return MintNowItem(
-                    id: index,
-                    url: mintNow[index]["url"].toString(),
-                    name: mintNow[index]["name"].toString().substring(0, 10) +
-                        "...",
-                    price: 2,
-                  );
-                },
-              )),
-          Container(
-            padding: EdgeInsets.fromLTRB(20, 20, 0, 0),
-            child: Text(
-              "Top Movers",
-              style: GoogleFonts.poppins(
-                  fontSize: 22,
-                  fontWeight: FontWeight.w800,
-                  color: context.watch<ThemeProvider>().getPriamryFontColor()),
-            ),
-          ),
-          TopMoversRowList(topMovers: topMoversMock),
-          Container(
-            padding: EdgeInsets.fromLTRB(20, 20, 0, 0),
-            child: Text(
-              "Live Drops",
-              style: GoogleFonts.poppins(
-                  fontSize: 22,
-                  fontWeight: FontWeight.w800,
-                  color: context.watch<ThemeProvider>().getPriamryFontColor()),
-            ),
-          ),
-          LiveDropRowList(liveDrops: liveDrops),
-          Container(
-            padding: EdgeInsets.fromLTRB(20, 20, 0, 0),
-            child: Text(
-              "Unique Items",
-              style: GoogleFonts.poppins(
-                  fontSize: 22,
-                  fontWeight: FontWeight.w800,
-                  color: context.watch<ThemeProvider>().getPriamryFontColor()),
-            ),
-          ),
-          Container(
-              height: 205,
-              width: 300,
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: mintNow.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return MintNowItem(
-                    id: index,
-                    url: mintNow[index]["url"].toString(),
-                    name: mintNow[index]["name"].toString().substring(0, 10) +
-                        "...",
-                    price: 2,
-                  );
-                },
-              )),
-        ],
-      ),
+          color: context.watch<ThemeProvider>().getBackgroundColor()),
     );
   }
 }
--- lib/screen/welcome/welcome_screen.dart
+++ lib/screen/welcome/welcome_screen.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:renderscan/common/theme/theme_provider.dart';
+import 'package:renderscan/common/utils/logger.dart';
+import 'package:renderscan/common/utils/storage.dart';
 import 'package:renderscan/constants.dart';
 import 'package:renderscan/screen/home/home_screen.dart';
 import 'package:lottie/lottie.dart';
+import 'package:renderscan/screen/login/login_screen.dart';
 
 class WelcomeScreen extends StatefulWidget {
   const WelcomeScreen({Key? key}) : super(key: key);
@@ -15,15 +18,25 @@ class WelcomeScreen extends StatefulWidget {
 class _WelcomeScreenState extends State<WelcomeScreen> {
   @override
   Widget build(BuildContext context) {
-    var future = Future.delayed(const Duration(milliseconds: 2000), () {
-      Navigator.push(
-          context, MaterialPageRoute(builder: (context) => HomeScreen()));
+    var future = Future.delayed(const Duration(milliseconds: 9000), () async {
+      var isUserLoggedIn = await Storage().getItem("username");
+      log.i(isUserLoggedIn);
+      if (isUserLoggedIn != null) {
+        return Navigator.push(
+            context, MaterialPageRoute(builder: (context) => HomeScreen()));
+      } else {
+        return Navigator.push(
+            context, MaterialPageRoute(builder: (context) => LoginScreen()));
+      }
     });
 
     future.then((value) {});
 
+    final size = MediaQuery.of(context).size;
+
     return Scaffold(
       body: Container(
+        width: size.width,
         color: context.watch<ThemeProvider>().getBackgroundColor(),
         child: Column(mainAxisAlignment: MainAxisAlignment.end, children: [
           Container(
